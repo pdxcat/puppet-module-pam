8cef3cd4c8892c558a6181e70497853a7333632f
Darin Perusich <darin@darins.net>
2013-02-04 16:23:16 -0500

    variable for config and added the header fragment

--- manifests/limits.pp
+++ manifests/limits.pp
@@ -12,11 +12,19 @@ define pam::limits (
     fail("pam::limits does not support osfamily $osfamily")
   }
 
-  realize Concat['/etc/security/limits.conf']
+  $limits_conf = "/etc/security/limits.conf"
+
+  realize Concat[$limits_conf]
+
+  concat::fragment { "limits_header":
+    target  => $limits_conf,
+    order   => 01,
+    content => template("pam/header.erb"),
+  }
 
   concat::fragment { "pam::limits ${domain}-${type}-${item}-${value}":
     ensure  => $ensure,
-    target  => '/etc/security/limits.conf',
+    target  => $limits_conf,
     content => "${domain} ${type} ${item} ${value}\n",
     order   => $priority,
   }
